2f5a152074e0b96ad9b944988c0a00166b2a7d89
Jussi Saurio <jussi.saurio@haltu.fi>
2017-05-22 11:54:13 +0300

    add and link react-native-fs for native filesystem access

--- android/app/src/main/java/com/ble_sovellus/MainApplication.java
+++ android/app/src/main/java/com/ble_sovellus/MainApplication.java
@@ -3,6 +3,7 @@ package com.ble_sovellus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import it.innove.BleManagerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage(),
             new BleManagerPackage()

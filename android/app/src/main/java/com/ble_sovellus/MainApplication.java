9786ba0f2579fce9db527803f90c3364e92f581b
Jussi Saurio <jussi.saurio@haltu.fi>
2017-06-05 14:28:47 +0300

    add and link react-native-zip-archive

--- android/app/src/main/java/com/ble_sovellus/MainApplication.java
+++ android/app/src/main/java/com/ble_sovellus/MainApplication.java
@@ -3,6 +3,7 @@ package com.ble_sovellus;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnziparchive.RNZipArchivePackage;
 import com.remobile.toast.RCTToastPackage;
 import com.ocetnik.timer.BackgroundTimerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNZipArchivePackage(),
             new RCTToastPackage(),
             new BackgroundTimerPackage(),
             new RNDeviceInfo(),

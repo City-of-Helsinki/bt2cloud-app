2ee00b1deb2b5e62f2d130f1b51285c1959996e3
Jussi Saurio <jussi.saurio@gmail.com>
2017-07-10 20:22:30 +0300

    added superduper awesome redux devtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import app from './reducers';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
-  let store = createStore(app, applyMiddleware(thunk));
+	// Change this to true to enable AWESOME redux remote devtools (http://remotedev.io/local/)
+	const composeEnhancers = composeWithDevTools({ realtime: false });
+  const store = createStore(app, composeEnhancers(applyMiddleware(thunk)));
   return store;
 };
